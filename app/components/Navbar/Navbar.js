f39f80f3348b7f74fb4de6c894fb3288c3410797
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-03-13 16:28:46 -0400

    Fix hover color for navbar items

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <nav className="flex justify-center gap-10 py-4 text-white   font-medium items-center">
-      <h2 className="hover:text-slate-400 cursor-pointer transition-all">
+      <h2 className="hover:text-slate-200 cursor-pointer transition-all">
         Questions
       </h2>
       <Image
@@ -13,7 +13,7 @@ export default function Navbar() {
         alt="Amora logo"
         className="relative bottom-1"
       />
-      <h2 className="hover:text-slate-400 cursor-pointer transition-all">
+      <h2 className="hover:text-slate-200 cursor-pointer transition-all">
         Pricing
       </h2>
     </nav>
